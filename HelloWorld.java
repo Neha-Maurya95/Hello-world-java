e70b22a0275414fc097eff6832ad595c56fe5715
Neha-Maurya95 <85605248+Neha-Maurya95@users.noreply.github.com>
2022-02-02 14:28:07 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,5 @@
-package com.sanumala;
-public class HelloWorld
-{   
-      public static void main(String[] args)
-      {       
-         System.out.println("Hello !! welcome to my first project !");   
-      }
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
 }
